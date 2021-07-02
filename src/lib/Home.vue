1d97cbfe32893ecd286ac98a050b4a65c663ff65
paul <abbb123@naver.com>
2021-07-02 14:42:43 +0900

    첫 커밋

---
+++ src/lib/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <ChatWindow/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import ChatWindow from './ChatWindow.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    ChatWindow
+  }
+}
+</script>
